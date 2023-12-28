a0b38e830db3bdede23e3d3fcf5b5fd39caf3dc0
Natanael Espinosa <natanael.espinosa@gmail.com>
2023-12-27 23:19:33 -0400

    Added "provision" component

--- components/http/resources/script.js
+++ components/http/resources/script.js
@@ -11,13 +11,16 @@ async function getHTMLComponent(component, data) {
         case "security":
             innerHTML = `<input type="text" name="" id="security-ssid" value="${data.ssid}">\
                         <input type="text" name="" id="security-password" value="${data.password}">\
+                        <h5 id="action-result"></h5>
                         <button id="security-button">Update</button>`
             element.innerHTML = innerHTML;
             document.getElementById("security-button").addEventListener("click", async function(event) {
+                let actionResult = document.getElementById("action-result");
                 let ssid = document.getElementById("security-ssid").value;
                 let password = document.getElementById("security-password").value;
                 let body = {"ssid": ssid, "password":password};
                 // await fetch('/configuration/security', {method: "POST", body : JSON.stringify(body)});
+                actionResult.innerText = JSON.stringify(body);
                 console.log(body);
             });
             break;
@@ -25,17 +28,43 @@ async function getHTMLComponent(component, data) {
             innerHTML = `<input type="number" name="" id="button-debounce" value="${data.debounceMs}">\
                         <input type="number" name="" id="button-actionDelay" value="${data.actionDelayMs}">\
                         <input type="number" name="" id="button-longPress" value="${data.longPressMs}">\
-                        <button id="button-configuration-button">Update</button>`
+                        <h5 id="action-result"></h5>
+                        <button id="configuration-button">Update</button>`
             element.innerHTML = innerHTML;
-            document.getElementById("button-configuration-button").addEventListener("click", async function(event) {
+            document.getElementById("configuration-button").addEventListener("click", async function(event) {
+                let actionResult = document.getElementById("action-result");
                 let debounceMs = document.getElementById("button-debounce").value;
                 let actionDelayMs = document.getElementById("button-actionDelay").value;
                 let longPressMs = document.getElementById("button-longPress").value;
                 let body = {"debounceMs": parseInt(debounceMs), "actionDelayMs": parseInt(actionDelayMs), "longPressMs": parseInt(longPressMs)};
-                console.log(body);
                 // await fetch('/configuration/security', {method: "POST", body : JSON.stringify(body)});
+                actionResult.innerText = JSON.stringify(body);
+                console.log(body);
             });
             break;
+        case "provision":
+            let options = "";
+            for (let entry of data) {
+                options = options + `<option value="${entry.ssid}">${entry.ssid}</option>`;
+            }
+            innerHTML = `<input type="text" name="product" list="ssid-list" id="provisioning-ssid" />
+                        <input type="text" id="provisioning-password"/>
+                        <datalist id="ssid-list">
+                        ${options} 
+                        </datalist>
+                        <h5 id="action-result"></h5>
+                        <button id="provision-button">Update</button>`
+            element.innerHTML = innerHTML;
+            document.getElementById("provision-button").addEventListener("click", async function(event) {
+                let actionResult = document.getElementById("action-result");
+                let ssid = document.getElementById("provisioning-ssid").value;
+                let password = document.getElementById("provisioning-password").value;
+                let body = {"ssid": ssid, "password": password};
+                // await fetch('/configuration/credentials', {method: "POST", body : JSON.stringify(body)});
+                actionResult.innerText = JSON.stringify(body);
+                console.log(body);
+            });
+            break
         default:
             innerHTML = ``;
             element.innerHTML = innerHTML;
@@ -48,8 +77,25 @@ async function loadComponent(component, arg) {
             const actionData = await getData(`/action/${arg}`);
             await getHTMLComponent(`action`, actionData);
             break;
+        case "provision": 
+            // const accessPointsList = await getData(`/ap`);
+            //TODO: get configured ssid and password to display as default values
+            const accessPointsList = [
+                {
+                    "ssid": "ssid1",
+                    "security": 1,
+                    "rssi": -23
+                }, 
+                {
+                    "ssid": "ssid2",
+                    "security": 1,
+                    "rssi": -23
+                }
+            ]
+            await getHTMLComponent("provision", accessPointsList);
+            break;
         case "security": 
-            // const buttonConfiguration = await getData(`/servercredentials`);
+            // const securityConfiguration = await getData(`/servercredentials`);
             const securityConfiguration = {
                 "ssid": "ssid",
                 "password": "password",
